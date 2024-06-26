ba75379ad329a29993143e4cd43e7c2352394bdf
blqcode <ertalb364@gmail.com>
2024-06-26 21:37:51 +0200

    JavaScript program to remove  duplicate in an array

---
+++ duplicateRemover.js
@@ -0,0 +1,23 @@
+
+function duplicate(a){
+a.forEach(i => {
+    let b=a.indexOf(i)
+    console.log(b)
+    if(a.length>=0){
+        if(b>=a.length){
+
+        
+        //compare the element of the array
+        if(b==b++){
+            a.splice(b)
+        }
+        else{
+            return;
+        }
+    }
+        
+}
+});
+}
+duplicate([1,2,3,1,2,4])
+Array
