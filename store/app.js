2da29e98b6207c1122ddb386486ef7e7a55618a0
blqcode <ertalb364@gmail.com>
2024-10-31 23:00:51 +0200

    updates

--- store/app.js
+++ store/app.js
@@ -2,18 +2,32 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const connectDB = require('./db/connectdb')
+const productRouter = require('./routes/products')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
 const app = express()
 
 //midlewares
 app.use(express.json())
 
 
+//routes
+app.use('api/v1/product',productRouter)
+app.get('/',(req,res)=>{
+    res.send(`</h1>Store API</h1><a href"api/v1/product">product route</a>`)
+})
 
 
+
+
+
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
 
 const start = async () =>{
    try {
-    const port = process.env.PORT
+    const port = process.env.PORT||3000
 
     await connectDB(process.env.MONGO_DB)
    
---
+++ store/controllers/products.js
@@ -0,0 +1,9 @@
+
+
+const getAllProducts = async (req,res) => {
+    res.status(200).json({msg:`product testing route`})
+}
+const getAllProductsStatic = async (req,res) => {
+    res.status(200).json({msg:`product testing route`})
+}
+module.exports= {getAllProducts,getAllProductsStatic}
