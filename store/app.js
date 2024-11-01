2a6f5a1a90013d4f27e55729b6be938505a1d35d
blqcode <ertalb364@gmail.com>
2024-11-01 20:32:48 +0200

    Update app.js

--- store/app.js
+++ store/app.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use('api/v1/',router)
 
 
-app.get('/',(req,res)=>{
+router.get('/',(req,res)=>{
     res.send(`</h1>Store API</h1><a href"api/v1/product">product route</a>`)
 })
 
