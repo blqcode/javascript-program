4094c9b45562654473051f688af9a182b3e2b75e
blqcode <ertalb364@gmail.com>
2024-11-04 23:59:26 +0200

    Update products.js

--- store/routes/products.js
+++ store/routes/products.js
@@ -4,6 +4,6 @@ const {getAllProducts,getAllProductsStatic} = require('../controllers/products')
 
 router.route('').get(getAllProducts)
 router.route('/static').get(getAllProductsStatic)
-
+ 
 
 module.exports= router
